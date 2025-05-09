499c2ea2bc0f71534ce150e8863f832b1a6ab78c
KrystianOg <krystian@ogonowski.it>
2025-05-09 21:25:32 +0200

    remove unused eslint rule

--- src/main.tsx
+++ src/main.tsx
@@ -15,9 +15,8 @@ declare module "@tanstack/react-router" {
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />,
-  </StrictMode>
+  </StrictMode>,
 );
